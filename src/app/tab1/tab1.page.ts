50d9938090fbeee7172615aaa5ebf1888155ec91
Yordy Manuel <salazarjavad@gmail.com>
2021-03-07 14:31:02 -0600

    fin de la unidad 2

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit{
+   
+  num:number;
 
   constructor() {}
+  
+  ngOnInit(){
+    console.log("Hello");
 
+    this.num = 12
+ 
+    this.num = this.num + 3
+
+    this.num = this.num + 5
+
+    console.log(this.num)
+  }
 }
